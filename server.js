6dd508e6542a2f640ea8daafaf5a7b479370f1b5
Maurice Stolk <info@mauricestolk.nl>
2022-09-14 15:57:38 +0000

    Nodes connecting right

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
+// DONNA
 const { port } = conf = {
 	proto: "http",
 	port: 3000,
 };
+let ids = [], clients = [],
+// activeNodes = [], 
+nodeListFromChain = [
+	`212.227.68.123`,
+	`81.169.159.30`
+];
 const server = require("socket.io")(port, {
 	cors: {
 		origin: ["*"],
 	},
 });
 server.on("connection", socket => {
-	console.log(socket.id);
+	// first time nobody knows so we add to list and tell everybody
+	ids.push(socket.id);
+	server.emit("joined-node", socket.id);
 });
 const io = require("socket.io-client");
-let nodeList = [
-	`212.227.68.123`
-];
-let clients = [];
+let broadcasts = [];
 const connectClients = () => {
-	nodeList.forEach((node,i) => {
+	nodeListFromChain.forEach((node,i) => {
 		const uri = `${conf.proto}://${node}:${port}`;
 		let socket = clients[i] = io(uri);
 		socket.on("connect", () => {
-			console.log("connected with ID: "+socket.id);
+			console.log(socket.id);
+		});
+		// If one of the servers tells me he has a new node, i tell this to my list
+		socket.on("joined-node", id => {
+			if (!ids.includes(id)) {
+				ids.push(id);
+				server.emit("joined-node", id);
+			};
 		});
 	});
 };
